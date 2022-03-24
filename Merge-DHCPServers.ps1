eade2991f64657568fe3375715af6d22c1addd57
Christopher Jordan Perry <92268654+jp-1297@users.noreply.github.com>
2022-03-24 07:16:30 -0500

    Create Merge-DHCPServers.ps1

---
+++ Merge-DHCPServers.ps1
@@ -0,0 +1,45 @@
+#>
+
+param (
+	[Parameter(Mandatory = $true)]
+	[string]$OldServer,
+	
+	[Parameter(Mandatory = $true)]
+	[string]$NewServer,
+	
+	[Parameter(Mandatory = $true)]
+	[pscredential]$Credential
+)
+
+#Checks if Powershell 7 to import DHCP module
+if ($PSVersionTable.psversion -like "7.*")
+{
+	Import-Module DhcpServer -ErrorAction Stop -skipeditioncheck
+}
+
+#Establish admin sessions with both DHCP Servers
+$OldDHCPCimSession = New-CimSession -Credential $Credential -ComputerName $OldServer
+$NewDHCPCimSession = New-CimSession -Credential $Credential -ComputerName $NewServer
+
+#Gather all the scopes in a variable
+$Scopes = Get-DhcpServerv4Scope -CimSession $OldDHCPCimSession
+
+#Pipe all the scopes to Add to new DHCP Server
+$Scopes | Add-DhcpServerv4Scope -CimSession $NewDHCPCimSession
+
+#Iterate through each scope to grab lease, options, and reservation info and add to new server scope
+foreach ($Scope in $Scopes)
+{
+	Get-DhcpServerv4OptionValue -ScopeId $Scope.ScopeID.IPAddressToString -CimSession $OldDHCPCimSession |
+	Set-DhcpServerv4OptionValue -ScopeId $Scope.ScopeID.IPAddressToString -CimSession $NewDHCPCimSession
+	
+	Get-DhcpServerv4Reservation -ScopeId $Scope.ScopeID.IPAddressToString -CimSession $OldDHCPCimSession |
+	Add-DhcpServerv4Reservation -ScopeId $Scope.ScopeID.IPAddressToString -CimSession $NewDHCPCimSession
+	
+	Get-DhcpServerv4Lease -ScopeId $Scope.ScopeId.IPAddressToString -CimSession $OldDHCPCimSession |
+	Where-Object -Property AddressState -EQ 'Active'|
+	Add-DhcpServerv4Lease -ScopeId $Scope.ScopeId.IpaddressToString -CimSession $NewDHCPCimSession
+	
+	$LeasesMoved = Get-DhcpServerv4Lease -ScopeId $Scope.ScopeID.IPAddressToString -CimSession $NewDHCPCimSession
+	Write-Output "Migrated $($LeasesMoved.Count) addresses from scope $($Scope.ScopeID.IPaddressToString)"
+}
